7c523401f3a8e3b0f74d02d146f8f2f360e5d4e4
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2024-10-31 06:21:43 +0100

    Delete app_pages/contents.py

--- app_pages/contents.py
+++
@@ -1,9 +0,0 @@
-# contents.py
-import streamlit as st
-from utils.csv_parser import load_csv_data
-
-def content_management():
-    st.header("Content Management")
-    data = load_csv_data("content_data.csv")
-    if data is not None:
-        st.write(data)
